af1e567779fdc0c2c7e9b919df1349368c9054ba
hdepoumps <depoumpshugo@gmail.com*>
2024-05-03 19:19:35 +0200

    Etape 1 et 2 finie

--- etape3/www/connect.php
+++ etape3/www/connect.php
@@ -5,5 +5,5 @@
     $db = "gestion_produits";
 
     $link = mysqli_connect($host,$username,$password,$db) or die ("Erreur de connexion à la base de données.");
-    mysqli_set_charset($link,"utf8");
+    mysqli_set_charset($link, "utf8mb4");
 ?>
